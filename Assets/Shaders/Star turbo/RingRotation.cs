968bc0021b2573be3d815957a3828bcd0af02bf6
Joaco <lopezjoaquin488@gmail.com>
2025-02-13 11:50:28 -0300

    scriopty

--- Assets/Shaders/Star turbo/RingRotation.cs
+++ Assets/Shaders/Star turbo/RingRotation.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelRotation : MonoBehaviour
+public class RingRotation : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 100f; // Velocidad de rotación en grados por segundo
 
     void Update()
     {
         // Rota el objeto en el eje X como si fuera una rueda
-        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
     }
 }
